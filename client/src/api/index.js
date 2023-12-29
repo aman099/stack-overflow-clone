0ae0762d7438bbf592349c05d6f29a0fa68e8280
aman <amandon.pathak12@gmail.com>
2023-12-29 16:29:03 +0530

    third commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://stack-overflow-clone-server-vrj4.onrender.com",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
